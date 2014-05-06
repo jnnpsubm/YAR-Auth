c9f4cbedbe26b6494a2acf9f0d60055a375f4093
jnnpsubm <jnnpsubm@hotmail.com>
2014-05-06 08:59:59 +0800

    small fix 2 reload cycle of profiles

--- YetAnotherRelogger/Resources/Plugin.cs
+++ YetAnotherRelogger/Resources/Plugin.cs
@@ -495,21 +495,7 @@ namespace YARPLUGIN
                 {
                     Log("Exception reading Coinage", 0);
                     _bs.Coinage = -1;
-                }
-
-                if (ZetaDia.IsInGame)
-                {
-                    _bs.LastGame = DateTime.Now.Ticks;
-                    _bs.IsInGame = true;
-                }
-                else
-                {
-                    if (_bs.IsInGame)
-                    {
-                        Send("GameLeft", true);
-                    }
-                    _bs.IsInGame = false;
-                }
+                }            
             }
             catch (Exception ex)
             {
@@ -552,6 +538,19 @@ namespace YARPLUGIN
 
                     _bs.IsPaused = BotMain.IsPaused;
 
+                    if (ZetaDia.IsInGame)
+                    {
+                        _bs.LastGame = DateTime.Now.Ticks;
+                        _bs.IsInGame = true;
+                    }
+                    else
+                    {
+                        if (_bs.IsInGame)
+                        {
+                            Send("GameLeft", true);
+                        }
+                        _bs.IsInGame = false;
+                    }
 
                     // Send stats
                     Send("XML:" + _bs.ToXmlString(), xml: true);
